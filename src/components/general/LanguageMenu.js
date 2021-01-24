ebe0f40a4061bd493eb80fe8c7a7340638b139dd
Cees <cjol99@icloud.com>
2021-01-24 13:46:39 +0100

    Small fixes

--- src/components/general/LanguageMenu.js
+++ src/components/general/LanguageMenu.js
@@ -21,7 +21,11 @@ const LanguageMenu = () => {
   return (
     // Disable eslint here for onBlur/onChange
     // eslint-disable-next-line jsx-a11y/no-onchange
-    <select value={language} onChange={(e) => handleChange(e)}>
+    <select
+      value={language}
+      onChange={(e) => handleChange(e)}
+      class="language-menu"
+    >
       {/* Disable eslint as option tags do not allow tags */}
       {/* eslint-disable jsx-a11y/accessible-emoji */}
       <option value="en">🇺🇸</option>
