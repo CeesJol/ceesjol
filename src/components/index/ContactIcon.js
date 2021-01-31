ab95a7f2bd6d526f5172b3718404829487576f4f
Cees <cjol99@icloud.com>
2021-01-31 17:36:46 +0100

    Link fix

--- src/components/index/ContactIcon.js
+++ src/components/index/ContactIcon.js
@@ -1,18 +1,30 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import "../../styles/components/contact-icon.scss";
 
 const ContactIcon = ({ contact }) => {
-  const { link, icon, download } = contact;
+  const { t } = useTranslation();
+  const { name, link, icon, download, translateLink, translateName } = contact;
+  const getLink = () => {
+    if (translateLink) return t(link);
+    return link;
+  };
+  const getTitle = () => {
+    if (translateName) return t(name);
+    return name;
+  };
   if (download) {
     return (
-      <a href={link} download>
-        <div className="contact-icon">{icon()}</div>
+      <a href={getLink()} download>
+        <div className="contact-icon" title={getTitle()}>
+          {icon()}
+        </div>
       </a>
     );
   }
   return (
-    <a href={link} target="_blank" rel="noreferrer">
-      <div className="contact-icon">
+    <a href={getLink()} target="_blank" rel="noreferrer">
+      <div className="contact-icon" title={getTitle()}>
         {/* <img src={`contact/${icon}.svg`} alt={`Link to my ${icon}`} /> */}
         {icon()}
       </div>
